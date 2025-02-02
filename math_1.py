78f395578b44f082b33acbd1640623667f7d722c
Pummy Kumari <153758643+pummy04@users.noreply.github.com>
2025-02-02 11:23:32 +0000

    Add files via upload

---
+++ math_1.py
@@ -0,0 +1,30 @@
+#square
+import math
+
+number = 25  # You can change this value
+square_root = math.sqrt(number)
+print(f"The square root of {number} is {square_root}")
+
+#factorial
+import math
+
+num = 6  # You can change this value
+fact = math.factorial(num)
+print(f"The factorial of {num} is {fact}")
+
+#value pf pi
+import math
+
+radius = 5  # You can change this value
+circumference = 2 * math.pi * radius
+print(f"The circumference of a circle with radius {radius} is {circumference}")
+
+#sine
+import math
+
+angle = 45  # You can change this value
+sine_value = math.sin(math.radians(angle))
+print(f"The sine of {angle} degrees is {sine_value}")
+
+
+
